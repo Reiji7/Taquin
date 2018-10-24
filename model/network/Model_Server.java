20821d0c90439e2a0a5b9b4179f3362d12e8c4f5
Adrien <adrien.claudel.info@gmail.com>
2018-10-24 16:42:25 +0200

    adding the server connection structure

---
+++ model/network/Model_Server.java
@@ -0,0 +1,34 @@
+package model.network;
+
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Connection;
+
+
+public class Model_Server implements Runnable{
+
+    private ServerSocket socketserver = null;
+    private Socket socket = null;
+    private Thread threadProc;
+
+	@Override
+	public void run() {
+		try {
+			socketserver = new ServerSocket(PORT);
+		    
+		    while (true) {
+		        socket = socketserver.accept();
+		        
+		        //créer un thread: pour échanger les données avec le client
+		        		        
+		        threadProc = new Thread();
+		        
+		        threadProc.start();
+		    }
+		} catch (IOException e) {
+		    System.err.println(e);
+		}		
+	}
+}
